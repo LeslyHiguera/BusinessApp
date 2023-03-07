f875cbfeba9c8d9543d54c96e5458e0dd50f26b7
LeslyHiguera <ldhiguera4@gmail.com>
2023-03-07 16:14:31 -0500

    Added base project

---
+++ BusinessAppUITests/BusinessAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BusinessAppUITestsLaunchTests.swift
+//  BusinessAppUITests
+//
+//  Created by Wilson David Molina Lozano on 6/03/23.
+//
+
+import XCTest
+
+final class BusinessAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
